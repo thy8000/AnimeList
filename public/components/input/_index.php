a02470ccd8b32937f6b57fe3eb8dccf17a64e459
thy8000 <thunaymoreiradesoares@gmail.com>
2025-03-11 00:54:15 -0300

    💄 ✨ Input component

---
+++ public/components/input/_index.php
@@ -0,0 +1,48 @@
+<?php
+
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+if (empty($args) || empty($args['id'])) {
+   return;
+}
+
+$type = $args['type'] ?? 'text';
+$id = $args['id'];
+$name = $args['name'] ?? $id;
+$label = $args['label'] ?? $args['label'];
+
+if ($type === 'select') {
+   $options = $args['options'] ?? [];
+
+   unset($args['options']);
+}
+
+unset($args['type']);
+unset($args['name']);
+unset($args['id']);
+unset($args['label']);
+
+$attrs = '';
+
+foreach ($args as $key => $value) {
+   $attrs .= ' ' . $key . '=' . $value . '';
+}
+
+if ($type === 'select') {
+   get_template_part('public/components/input/components/select', null, [
+      'id'   => $id,
+      'name' => $name,
+      'label' => $label,
+      'attrs' => $attrs,
+      'options' => $options,
+   ]);
+} else {
+   get_template_part('public/components/input/components/text', null, [
+      'id'   => $id,
+      'name' => $name,
+      'label' => $label,
+      'attrs' => $attrs,
+   ]);
+}
