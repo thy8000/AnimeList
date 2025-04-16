974d3599c8bbb70b2fcfa8b62cdee87e9652e227
thy8000 <thunaymoreiradesoares@gmail.com>
2025-04-16 00:58:19 -0300

    🤡 Virtual page anime: Header

--- public/pages/virtual-page-anime/_index.php
+++ public/pages/virtual-page-anime/_index.php
@@ -4,4 +4,6 @@ get_template_part('public/components/header/_index', null, [
    'x-data' => 'virtualPageAnime'
 ]);
 
+get_template_part('public/pages/virtual-page-anime/components/header');
+
 get_template_part('public/components/footer/_index');
---
+++ public/pages/virtual-page-anime/components/header.php
@@ -0,0 +1,37 @@
+<?php
+
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+?>
+
+<section class="mt-20">
+   <div class="custom-container">
+      <div class="flex lg:flex-row flex-col gap-8">
+         <div class="lg:basis-1/4">
+            <picture>
+               <img class="aspect-[2/0] h-96" src="https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx137595-1m3i7vxyOqXc.png">
+            </picture>
+         </div>
+
+         <div class="flex flex-col">
+            <h1 class="text-3xl font-poppins font-semibold text-green-500">Naruto</h1>
+
+            <h2 class="mt-1 text-xl font-semibold text-white">66666년만에 환생한 흑마법사</h2>
+
+            <span class="mt-4 self-start bg-green-500 rounded-lg py-1 px-2 text-white text-sm">
+               Anime
+            </span>
+
+            <div class="flex flex-col mt-10 text-white">
+               <span class="border-y border-neutral-700 border-solid py-2"><b>Status:</b> Completo</span>
+               <span class="border-y border-neutral-700 border-solid py-2"><b>Episódios/Capítulos:</b> 220</span>
+               <span class="border-y border-neutral-700 border-solid py-2"><b>Data de lançamento:</b> 20 de outubro de 2024</span>
+               <span class="border-y border-neutral-700 border-solid py-2"><b>Estúdio/Autor:</b> Studio Pierrot</span>
+               <span class="border-y border-neutral-700 border-solid py-2"><b>Gêneros:</b> Ação, Comédia, Hentai, Aventura</span>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
